5a58cea9c06defa3809218b83e8b89204fb13d0d
Dragesia <brkgzlsy@gmail.com>
2022-10-02 23:56:22 +0300

    added some functions to script

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const addCard = document.querySelector(".add");
 const addButton = document.querySelector(".add-js");
 const cancelButton = document.querySelector(".cancel");
 
+const bookName = document.querySelector("input[name=bookname]");
+const author = document.querySelector("input[name=author]");
+const pages = document.querySelector("input[name=pages]");
+const read = document.querySelector("input[name=read]");
+
 addCard.onclick = () => modal.style.display = "flex";
 cancelButton.onclick = () => modal.style.display = "";
+
+let myLibrary = [];
+
+function addBook() {
+    const newBook = new Book(bookName.value, author.value, pages.value, read.value);
+
+    myLibrary.push(newBook);
+}
+
+function resetGrid() {
+    
+}
 
 function Book(title, author, pages, status) {
     this.title = title
