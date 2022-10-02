e9ff73ff92de7524eb41ed5bfa997b07a35fb2e0
Dragesia <brkgzlsy@gmail.com>
2022-10-02 23:27:54 +0300

    created login page

--- script.js
+++ script.js
@@ -7,7 +7,3 @@ function Book(title, author, pages, status) {
         return title + " by " + author + ", " + pages + " pages, " + status
     }
 }
-
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
-
-console.log(theHobbit.info())
