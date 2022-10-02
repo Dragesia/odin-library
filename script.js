262643817c03d67a940570f35c0f44f8d6546f00
Dragesia <brkgzlsy@gmail.com>
2022-10-02 23:44:45 +0300

    added script for modal add page

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const modal = document.querySelector(".modal");
+const addCard = document.querySelector(".add");
+const addButton = document.querySelector(".add-js");
+const cancelButton = document.querySelector(".cancel");
+
+addCard.onclick = () => modal.style.display = "flex";
+cancelButton.onclick = () => modal.style.display = "";
+
 function Book(title, author, pages, status) {
     this.title = title
     this.author = author
     this.pages = pages
     this.status = status
-    this.info = function() {
-        return title + " by " + author + ", " + pages + " pages, " + status
-    }
 }
